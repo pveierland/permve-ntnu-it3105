f5ee53999368948ff0704bb15fd0af22e2a24cee
Per Magnus Veierland <pveierland@gmail.com>
2015-09-27 16:18:05 +0200

    vi: Updated Graph.lookup to allow matching with function predicate

--- vi/vi/graph/graph.py
+++ vi/vi/graph/graph.py
@@ -13,7 +13,7 @@ class Graph(object):
 
     def __str__(self):
         return '\n'.join(str(e) for e in self.edges)
-    
+
     def get_vertex(self, x):
         return x if x is vi.graph.Vertex else self.lookup(x) or self.insert_vertex(x)
 
@@ -21,7 +21,7 @@ class Graph(object):
         v = x if x is vi.graph.Vertex else vi.graph.Vertex(x)
         self.vertices.add(v)
         return v
-    
+
     def link(self, a, b, cost=None):
         v1 = self.get_vertex(a)
         v2 = self.get_vertex(b)
@@ -33,5 +33,10 @@ class Graph(object):
 
         return e
 
-    def lookup(self, value):
-        return next((v for v in self.vertices if v.value == value), None)
+    def lookup(self, predicate):
+        if hasattr(predicate, '__call__'):
+            matches = filter(predicate, self.vertices)
+        else:
+            matches = [v for v in self.vertices if v.value == value]
+        assert len(matches) <= 1
+        return matches[0]
