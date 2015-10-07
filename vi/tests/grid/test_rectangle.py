1270cfbdabbb3e7e9ff73ab9975bb79ad4c2c71c
Per Magnus Veierland <pveierland@gmail.com>
2015-10-07 16:47:40 +0200

    vi: Rectangle class WIP

---
+++ vi/tests/grid/test_rectangle.py
@@ -0,0 +1,44 @@
+from vi.grid import Coordinate, Rectangle
+
+from nose.tools import assert_equals
+
+#o o o o o o
+#o o o o o o
+#o o o x x o
+#o o o x x o
+#o o o x x o
+#o o o x x o
+#o o o o o o
+
+def test_intersects_coordinate():
+    rectangle = Rectangle(3, 2, 2, 4)
+
+    assert rectangle.intersects_coordinate(Coordinate(3, 2))
+    assert rectangle.intersects_coordinate(Coordinate(3, 3))
+    assert rectangle.intersects_coordinate(Coordinate(3, 4))
+    assert rectangle.intersects_coordinate(Coordinate(3, 5))
+    
+    assert rectangle.intersects_coordinate(Coordinate(4, 2))
+    assert rectangle.intersects_coordinate(Coordinate(4, 3))
+    assert rectangle.intersects_coordinate(Coordinate(4, 4))
+    assert rectangle.intersects_coordinate(Coordinate(4, 5))
+
+    assert not rectangle.intersects_coordinate(Coordinate(2, 1))
+    assert not rectangle.intersects_coordinate(Coordinate(2, 2))
+    assert not rectangle.intersects_coordinate(Coordinate(2, 3))
+    assert not rectangle.intersects_coordinate(Coordinate(2, 4))
+    assert not rectangle.intersects_coordinate(Coordinate(2, 5))
+    assert not rectangle.intersects_coordinate(Coordinate(2, 6))
+    
+    assert not rectangle.intersects_coordinate(Coordinate(3, 1))
+    assert not rectangle.intersects_coordinate(Coordinate(3, 6))
+    
+    assert not rectangle.intersects_coordinate(Coordinate(4, 1))
+    assert not rectangle.intersects_coordinate(Coordinate(4, 6))
+    
+    assert not rectangle.intersects_coordinate(Coordinate(5, 1))
+    assert not rectangle.intersects_coordinate(Coordinate(5, 2))
+    assert not rectangle.intersects_coordinate(Coordinate(5, 3))
+    assert not rectangle.intersects_coordinate(Coordinate(5, 4))
+    assert not rectangle.intersects_coordinate(Coordinate(5, 5))
+    assert not rectangle.intersects_coordinate(Coordinate(5, 6))
--- vi/vi/grid/rectangle.py
+++ vi/vi/grid/rectangle.py
@@ -17,3 +17,29 @@ class Rectangle(object):
     def __str__(self):
         return "Rectangle(x={0},y={1},width={2},height={3})" \
             .format(self.x, self.y, self.width, self.height)
+
+    def distance_to_coordinate(self, coordinate):
+        end_x = self.coordinate.x + self.width - 1
+        end_y = self.coordinate.y + self.height - 1
+
+        if coordinate.x < self.x:
+            diff_x = self.x - coordinate.x
+        elif coordinate.x > end_x:
+            diff_x = coordinate.x - end_x
+        else:
+            diff_x = 0
+
+        if coordinate.y < self.y:
+            diff_y = self.y - coordinate.y
+        elif coordinate.y > end_y:
+            diff_y = coordinate.y - end_y
+        else:
+            diff_y = 0
+
+        return (diff_x, diff_y)
+
+    def intersects_coordinate(self, coordinate):
+        return (self.x <= coordinate.x) and \
+               (coordinate.x < self.x + self.width) and \
+               (self.y <= coordinate.y) and \
+               (coordinate.y < self.y + self.height)
