17d2bb84c9e8184e4944f9a1605468a8cd12ec84
Per Magnus Veierland <pveierland@gmail.com>
2015-10-27 03:34:19 +0100

    M4: Cleaned board code

--- module_4/program/vi_2048_board.cpp
+++ module_4/program/vi_2048_board.cpp
@@ -28,42 +28,25 @@ namespace vi
     {
         std::vector<board::heuristic> heuristic_lut(board::lut_size);
 
-        const float SCORE_LOST_PENALTY        = 200000.0;
-        const float SCORE_MONOTONICITY_POWER  = 4.0;
-        const float SCORE_MONOTONICITY_WEIGHT = 47.0;
-        const float SCORE_SUM_POWER           = 3.5;
-        const float SCORE_SUM_WEIGHT          = 11.0;
-        const float SCORE_MERGES_WEIGHT       = 700.0;
-        const float SCORE_EMPTY_WEIGHT        = 270.0;
-
         for (unsigned row = 0; row < 65536; ++row)
         {
             unsigned line[4] = {
-                (row >> 0) & 0xf,
-                (row >> 4) & 0xf,
-                (row >> 8) & 0xf,
-                (row >> 12) & 0xf
+                (row >> 0)  & 0xFU,
+                (row >> 4)  & 0xFU,
+                (row >> 8)  & 0xFU,
+                (row >> 12) & 0xFU
             };
-
-            int empty = 0;
-
-            for (int i = 0; i < 4; ++i)
-            {
-                if (line[i] == 0)
-                {
-                    ++empty;
-                }
-            }
 
             const bool first_two  = line[0] && line[0] == line[1];
             const bool middle_two = line[1] && line[1] == line[2];
             const bool last_two   = line[2] && line[2] == line[3];
-            const bool first_last = line[0] && line[0] == line[3] && line[1] == 0 && line[2] == 0;
+            const bool first_last = line[0] && line[0] == line[3] && \
+                                    line[1] == 0 && line[2] == 0;
 
-            int num_merges = 0;
-            int bad_tiles = 0;
+            int num_merges    = 0;
+            int bad_tiles     = 0;
             int second_merges = 0;
-            int third_merges = 0;
+            int third_merges  = 0;
 
             if (first_two && last_two)
             {
@@ -215,105 +198,11 @@ namespace vi
                 }
             }
 
-//            heuristic_lut[row] = 10.0 + num_merges * 0.7 + 0.2 * second_merges + 0.1 * third_merges - pow(5, bad_tiles) / 100.0; //GOOD 15 min 4K @ depth 7
-
-//            heuristic_lut[row] =
-//                0.3
-//                + 0.25 * num_merges
-//                + 0.15 * second_merges
-//                + 0.05 * third_merges
-//                - pow(5, bad_tiles) / 3125.0;
-            
-            // 61596
-//            heuristic_lut[row] =
-//                5.0
-//                + 0.5 * num_merges
-//                + 0.15 * second_merges
-//                + 0.05 * third_merges
-//                - 5.0 * pow(5, bad_tiles) / 3125.0;
-           
-            // 37020
-            // 61524
-            heuristic_lut[row] =
-                5.0
-                + 0.5 * num_merges
-                + 0.15 * second_merges
-                + 0.05 * third_merges
-                - 8.0 * pow(5, bad_tiles) / 3125.0;
-            
-            // 37372
-            heuristic_lut[row] =
-                5.0
-                + 0.5 * num_merges
-                + 0.15 * second_merges
-                + 0.05 * third_merges
-                - 10.0 * pow(5, bad_tiles) / 3125.0;
-            
-            heuristic_lut[row] =
-                100.0
-                + 10.0 * num_merges
-                + 5.0 * second_merges
-                + 2.5 * third_merges
-                - 50.0 * pow(5, bad_tiles) / 625.0;
-
-            //heuristic_lut[row] = 0.2 + num_merges * 0.5 + 0.2 * second_merges + 0.1 * third_merges - pow(2, bad_tiles) / 16.0; BAD
-            //heuristic_lut[row] = 0.45 + num_merges * 0.2 + 0.1 * second_merges + 0.05 * third_merges - pow(2, bad_tiles) / 160.0; // ISH
-
-
-
-            //heuristic_lut[row] = 10000 + 100 * num_merges
-//                + 50 * second_merges + 25 * third_merges;// -pow(5, bad_tiles);
-
-            /*
-
-
-            // Heuristic score
-            float sum = 0;
-            int empty = 0;
-            int merges = 0;
-
-            int prev = 0;
-            int counter = 0;
-            for (int i = 0; i < 4; ++i) {
-                int rank = line[i];
-                sum += pow(rank, SCORE_SUM_POWER);
-                if (rank == 0) {
-                    empty++;
-                }
-                else {
-                    if (prev == rank) {
-                        counter++;
-                    }
-                    else if (counter > 0) {
-                        merges += 1 + counter;
-                        counter = 0;
-                    }
-                    prev = rank;
-                }
-            }
-            if (counter > 0) {
-                merges += 1 + counter;
-            }
-
-            float monotonicity_left = 0;
-            float monotonicity_right = 0;
-            for (int i = 1; i < 4; ++i) {
-                if (line[i - 1] > line[i]) {
-                    monotonicity_left += pow(line[i - 1], SCORE_MONOTONICITY_POWER) - pow(line[i], SCORE_MONOTONICITY_POWER);
-                }
-                else {
-                    monotonicity_right += pow(line[i], SCORE_MONOTONICITY_POWER) - pow(line[i - 1], SCORE_MONOTONICITY_POWER);
-                }
-            }
-
-            heuristic_lut[row] = empty;
-
-            heuristic_lut[row] = SCORE_LOST_PENALTY +
-                SCORE_EMPTY_WEIGHT * empty +
-                SCORE_MERGES_WEIGHT * merges -
-                SCORE_MONOTONICITY_WEIGHT * std::min(monotonicity_left, monotonicity_right) -
-                SCORE_SUM_WEIGHT * sum;
-            */
+            heuristic_lut[row] = 100.0
+                               + 10.0 * num_merges
+                               +  5.0 * second_merges
+                               +  2.5 * third_merges
+                               - 50.0 * pow(5, bad_tiles) / 625.0;
         }
 
         return heuristic_lut;
